862c29da218f0545c9c930e4400278b6e45d7e55
Manu Cheremeh <manucheremeh1995@gmail.com>
2020-12-30 16:34:23 +0000

    fix:Making find_or_create a self method

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,12 +1,12 @@
 class Identity < ApplicationRecord
   belongs_to :user
 
-  def find_or_create(auth)
+  def self.find_or_create(auth)
     unless auth_user = find_by_provider_and_uid(auth['provider'], auth['uid'])
       user = User.create name: auth['info']['name'], email: auth['info']['email']
       auth_user = User.create user: user, uid: auth['uid'], provider: auth['provider']
     end
-    
+
     auth_user
   end
 end
